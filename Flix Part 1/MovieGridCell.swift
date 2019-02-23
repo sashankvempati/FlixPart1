0577f717c2641bea254ae74c9cb133db28977402
Sashank Vempati <sashankvempati@Sashanks-MacBook-Pro.local>
2019-02-22 20:04:14 -0800

    Flix p2

---
+++ Flix Part 1/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  Flix Part 1
+//
+//  Created by Sashank Vempati on 2/22/19.
+//  Copyright © 2019 Sashank Vempati. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
